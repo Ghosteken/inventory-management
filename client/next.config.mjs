5ebd8da1d3405bf564174d94104c1a55105185d6
nicholas <aigberuan6@gmail.com>
2025-10-31 12:26:18 +0100

    init2

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  images: {},
 };
 
 export default nextConfig;
--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -67,11 +67,12 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
-              alt="Profile"
-              width={50}
-              height={50}
-              className="rounded-full h-full object-cover"
+              // ...
+              src="/assets/profile.jpg"
+              alt="profile"
+              width={36}
+              height={36}
+              className="rounded-full"
             />
             <span className="font-semibold">Ed Roh</span>
           </div>
